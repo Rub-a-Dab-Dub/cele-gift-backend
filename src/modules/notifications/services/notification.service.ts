e0875045a5c2001bfe8f68eb828fab28344e9e0e
llins <llinsomoudu@gmail.com>
2025-05-29 13:14:40 +0100

    exportclass

--- src/modules/notifications/services/notification.service.ts
+++ src/modules/notifications/services/notification.service.ts
@@ -9,3 +9,13 @@ import { CreateNotificationDto } from '../dto/create-notification.dto';
 import { GetNotificationsDto } from '../dto/get-notifications.dto';
 import { PaginatedResult } from '../interfaces/notification.interfaces';
 import { NotificationType } from '../enums/notification.enums';
+@Injectable()
+export class NotificationService {
+  constructor(
+    @InjectRepository(Notification)
+    private notificationRepo: Repository<Notification>,
+    @InjectRepository(UserNotificationPreference)
+    private preferenceRepo: Repository<UserNotificationPreference>,
+    private queueService: NotificationQueueService,
+    private templateService: NotificationTemplateService,
+  ) {}
